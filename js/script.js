3c0bc9e2142c1dc95c1279932d78eaeca4524786
hycienth paul <hycienthp025@gmail.com>
2025-02-15 18:35:09 +0100

    flexible

--- js/script.js
+++ js/script.js
@@ -3,20 +3,24 @@
 const sec = document.getElementById("seconds");
 const min = document.getElementById("minutes");
 const hrs = document.getElementById("hours");
+const ampm = document.getElementById("amPm");
 
 setInterval(() => {
+  const currentTime = new Date();
+  const hour = currentTime.getHours();
 
-    const currentTime = new Date();
+  hrs.innerHTML = (hour < 10 ? "0" : "") + hour;
 
-    hrs.innerHTML= (currentTime.getHours() < 10?'0' : '') + currentTime.getHours();
+  min.innerHTML =
+    (currentTime.getMinutes() < 10 ? "0" : "") + currentTime.getMinutes();
 
-    min.innerHTML= (currentTime.getMinutes() < 10?'0':'') + currentTime.getMinutes();
+  sec.innerHTML =
+    (currentTime.getSeconds() < 10 ? "0" : "") + currentTime.getSeconds();
 
-    sec.innerHTML= (currentTime.getSeconds() < 10?'0':'') + currentTime.getSeconds();
-
+  ampm.innerHTML = hour >= 12 ? "PM" : "AM";
 }, 1000);
 
-// for the date 
+// for the date
 
 const mnth = document.getElementById("month");
 const day = document.getElementById("day");
@@ -26,12 +30,12 @@ const date = new Date();
 
 year.textContent = date.getFullYear();
 dayNum.textContent = date.getDate();
-day.textContent = date.toLocaleString("en",{
-     weekday: "long"
-})
+day.textContent = date.toLocaleString("en", {
+  weekday: "long",
+});
 
-mnth.textContent = date.toLocaleString("en",{
-    month: "long"
-})
+mnth.textContent = date.toLocaleString("en", {
+  month: "long",
+});
 
 console.log(date.getDate());
